c6e789af25f9eaeae354d751a1f04fc4791aa9bb
Ekene <engrkenzyobi@gmail.com>
2022-10-18 16:35:24 -0700

    u

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 
-#ifndef main_h
-#define main_h
-int _putchar(char);
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
