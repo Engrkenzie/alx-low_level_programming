38172759d35b5969aeb51f5fdb9a425ba1ba9f6d
Ekene <engrkenzyobi@gmail.com>
2022-10-18 15:16:51 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Prints _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
