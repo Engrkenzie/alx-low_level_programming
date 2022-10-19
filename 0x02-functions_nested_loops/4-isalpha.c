ef074a8339f27208b1d8a7a940d90f42dde6d27d
Ekene <engrkenzyobi@gmail.com>
2022-10-18 17:42:28 -0700

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_isalpha - chexks for char alphabet
+ *@c: the char ro be checked
+ *Return: 1 if chat is letter lower or upper, 0 otherwise
+*/
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
