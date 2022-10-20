2a4228f7989d648a3ed158205db40609a8a22400
Ekene <engrkenzyobi@gmail.com>
2022-10-20 14:34:20 -0700

    task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -4,6 +4,7 @@
  * print_number - Prints an integer.
  * @n: The integer to be printed.
  */
+
 void print_number(int n)
 {
 	unsigned int num = n;
@@ -19,5 +20,3 @@ void print_number(int n)
 
 	_putchar((num % 10) + '0');
 }
-Footer
-
