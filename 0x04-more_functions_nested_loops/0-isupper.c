5ecb2ffbe299c18d19cea4bcfba6c75fa5e27318
Ekene <engrkenzyobi@gmail.com>
2022-10-20 12:41:14 -0700

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,10 +6,9 @@
 
 int _isupper(int c);
 {
-	char c;
-	c = 'A';
-	printf("%c: %d\n", c, _isupper(c));
-	c = 'a';
-	printf("%c: %d\n", c, _isupper(c));
-	return (0);
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+
+	else
+		return (0);
 }
