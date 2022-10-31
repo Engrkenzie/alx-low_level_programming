03e8d60af7aef8ed59285d63add09f9369b207ef
Ekene <engrkenzyobi@gmail.com>
2022-10-31 15:05:16 -0700

    task

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -11,6 +11,7 @@
 
 int _putchar(char c);
 void *_memset(void *s, int c, size_t n);
+void *_memcpy(void *dest, const void *src, size_t n)
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
