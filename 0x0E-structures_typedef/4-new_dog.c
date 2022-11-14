8c2368d161e764eef76f8584cd2517bca050098c
Ekene <engrkenzyobi@gmail.com>
2022-11-14 14:41:26 -0800

    0x0E strucrures

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -22,12 +22,12 @@ int _strlen(char *str)
 }
 
 /**
- * _strcopy - copies a string pointed to by src, including the
+ * _strcopy - Copies a string pointed to by src, including the
  *            terminating null byte, to a buffer pointed to by dest.
- * @dest: the buffer storing the string copy.
- * @src: the source string.
+ * @dest: The buffer storing the string copy.
+ * @src: The source string.
  *
- * return: the pointer to dest.
+ * Return: The pointer to dest.
  */
 char *_strcopy(char *dest, char *src)
 {
@@ -42,37 +42,37 @@ char *_strcopy(char *dest, char *src)
 }
 
 /**
- * new_dog - creates a new dog.
- * @name: the name of the dog.
- * @age: the age of the dog.
- * @owner: the owner of the dog.
+ * new_dog - Creates a new dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
  *
- * return: the new struct dog.
+ * Return: The new struct dog.
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *doggo;
 
-	if (name == null || age < 0 || owner == null)
-		return (null);
+	if (name == NULL || age < 0 || owner == NULL)
+		return (NULL);
 
 	doggo = malloc(sizeof(dog_t));
-	if (doggo == null)
-		return (null);
+	if (doggo == NULL)
+		return (NULL);
 
 	doggo->name = malloc(sizeof(char) * (_strlen(name) + 1));
-	if (doggo->name == null)
+	if (doggo->name == NULL)
 	{
 		free(doggo);
-		return (null);
+		return (NULL);
 	}
 
 	doggo->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
-	if (doggo->owner == null)
+	if (doggo->owner == NULL)
 	{
 		free(doggo->name);
 		free(doggo);
-		return (null);
+		return (NULL);
 	}
 
 	doggo->name = _strcopy(doggo->name, name);
